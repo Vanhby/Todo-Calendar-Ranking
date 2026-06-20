085ab54ca2f0093e4c6fdddeaea4a57c85706d19
= <lethanhvietanh2005@gmail.com>
2026-06-20 10:53:33 +0700

    add Cors

--- StreakHub.API/StreakHub.API/Program.cs
+++ StreakHub.API/StreakHub.API/Program.cs
@@ -3,6 +3,16 @@ using StreakHub.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -11,6 +21,8 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
