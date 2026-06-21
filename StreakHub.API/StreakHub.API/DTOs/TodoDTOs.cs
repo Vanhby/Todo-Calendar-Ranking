de4fd1bd9aefaa014a9cb36d424fae1e9afaa1ca
ZkinerVN <kang24676@gmail.com>
2026-06-21 18:27:46 +0700

    feat: hoàn thành TodoDTOs

---
+++ StreakHub.API/StreakHub.API/DTOs/TodoDTOs.cs
@@ -0,0 +1,27 @@
+﻿namespace StreakHub.API.DTOs
+{
+    public class TodoDTOs
+    {
+        // Dùng cho 5 
+        public class TodoCreateRequest
+        {
+            public string Title { get; set; } = string.Empty;
+            public string Date { get; set; } = string.Empty;
+        }
+
+        //7 
+        public class TodoUpdateRequest
+        {
+            public string Title { get; set; } = string.Empty;
+            public bool IsCompleted { get; set; }
+        }
+
+      //9 
+        public class TodoResponse
+        {
+            public int Id { get; set; }
+            public string Title { get; set; } = string.Empty;
+            public bool IsCompleted { get; set; }
+        }
+    }
+}
