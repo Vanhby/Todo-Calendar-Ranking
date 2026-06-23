85bb23a08e92efa228341c0f78bb9e48fd9168d7
ZkinerVN <kang24676@gmail.com>
2026-06-23 17:00:50 +0700

    chore: cập nhật model Tag thêm thuộc tính Color

--- StreakHub.API/StreakHub.API/Models/Tag.cs
+++ StreakHub.API/StreakHub.API/Models/Tag.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
 
+        // Thêm trường Color để hứng mã màu từ API (Endpoint 12)
+        public string Color { get; set; } = "#8b949e";
+
         public virtual ICollection<TodoTag> TodoTags { get; set; } = new List<TodoTag>();
         public virtual ICollection<ShareTag> ShareTags { get; set; } = new List<ShareTag>();
     }
